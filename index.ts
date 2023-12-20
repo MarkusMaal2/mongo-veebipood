160560eebdd7960ae652aba0dd6cdf6dc5837fc8
MarkusMaal2 <markus.maal@voco.ee>
2023-12-20 13:51:11 +0200

    Add project files and dependencies

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb+srv://markusmaal:A9bpQ9RbUSTbZ34o@cluster0.u5k9lk4.mongodb.net/?retryWrites=true&w=majority");
+const database = mongoose.connection;
+database.on('error', (error) => {
+    console.log(error);
+})
+
+database.once('connected', () => {
+    console.log("Database Connected")
+})
+
+// create application/json parser
+const app: Express = express();
+app.use(bodyParser.json());
+app.get('/', (req:Request, res:Response) =>{
+    res.send("Express + TypeScript Server");
+})
+
+app.listen(3001, () => {
+    console.log(`[server]: Server is running at http://localhost:3001`)
+})
