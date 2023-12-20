1566444c746087ee46e633f4fe8ab3a56e5b2c8f
MarkusMaal2 <markus.maal@voco.ee>
2023-12-20 14:10:49 +0200

    Category controller

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
+import categoryController from "./controllers/categoryController"
 
 import mongoose from "mongoose"
 
@@ -16,9 +17,10 @@ database.once('connected', () => {
 // create application/json parser
 const app: Express = express();
 app.use(bodyParser.json());
-app.get('/', (req:Request, res:Response) =>{
+app.use('/', categoryController)
+/*app.get('/', (req:Request, res:Response) =>{
     res.send("Express + TypeScript Server");
-})
+})*/
 
 app.listen(3001, () => {
     console.log(`[server]: Server is running at http://localhost:3001`)
